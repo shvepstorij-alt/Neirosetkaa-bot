db14366398490e5cfcf0e22f41eef240dcd6f639
shvepstorij-alt <shvepstorij@gmail.com>
2026-04-06 13:01:03 +0500

    Delete tg_bot_alexandr_ready/tg_bot directory

--- tg_bot_alexandr_ready/tg_bot/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")           # Токен от @BotFather
-CLAUDE_API_KEY = os.getenv("CLAUDE_API_KEY") # Ключ Anthropic API
-CHANNEL_ID = os.getenv("CHANNEL_ID")         # ID канала, например -1001234567890
-ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "AleksandrOii")  # Без @
